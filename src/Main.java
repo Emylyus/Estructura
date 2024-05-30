49e58bb2cec194a63566c6b9be56713a022eedc1
Emylyus <emilydanielaespinoza@gmail.com>
2024-05-30 00:26:42 -0500

    [4.0]

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        ObjetcControlador metodo = new ObjetcControlador();
         int[] Array = {42, 17, 23, 56, 89, 3, 14};
 
         // Imprimir los valores del array
         for (int i = 0; i < Array.length; i++) {
             System.out.println("Elemento en el índice " + i + ": " + Array[i]);
         }
+        metodo.ordenarBurbuja(Array);
+        metodo.imprimirArray(Array);
     }
 }
